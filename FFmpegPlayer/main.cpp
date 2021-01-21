865c8aa0aa45de69486aaafc326b111235967ad9
guolx <guolinxing@oppo.com>
2021-01-21 09:22:26 +0800

    Initial Commit

---
+++ FFmpegPlayer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FFmpegPlayer
+//
+//  Created by 郭林杏(Shirley) on 2021/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
